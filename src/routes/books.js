30a61907eeb5135eaf01cd0289d8f0edf204e84d
Victor Li <victorli5674@yahoo.com>
2017-10-13 22:19:38 -0700

    made a route to for getting pages for a certain book

--- src/routes/books.js
+++ src/routes/books.js
@@ -23,30 +23,24 @@ router.get('/search', (req, res) => {
         })
       )
     );
-  // res.json({
-  //   books: [
-  //     {
-  //       goodreadsId: 1,
-  //       title: '1984',
-  //       authors: 'Orwell',
-  //       covers: [
-  //         'https://images.gr-assets.com/books/1348990566l/5470.jpg',
-  //         'https://images.gr-assets.com/books/1504611957l/9577857.jpg'
-  //       ],
-  //       pages: 198
-  //     },
-  //     {
-  //       goodreadsId: 2,
-  //       title: 'Three Men in a Boat',
-  //       authors: 'Jerome K. Jerome',
-  //       covers: [
-  //         'https://images.gr-assets.com/books/1392791656l/4921.jpg',
-  //         'https://images.gr-assets.com/books/1312036878l/627830.jpg'
-  //       ],
-  //       pages: 256
-  //     }
-  //   ]
-  // });
+});
+
+router.get('/fetchPages', (req, res) => {
+  const goodreadsId = req.query.goodreadsId;
+
+  request
+    .get(
+      `https://www.goodreads.com/book/show.xml?key=${process.env.GOODREADS_API}&id=${goodreadsId}`
+    )
+    .then(result =>
+      parseString(result, (err, goodreadsResult) => {
+        const numPages = goodreadsResult.GoodreadsResponse.book[0].num_pages[0];
+        const pages = numPages ? parseInt(numPages, 10) : 0;
+        res.json({
+          pages
+        });
+      })
+    );
 });
 
 export default router;
